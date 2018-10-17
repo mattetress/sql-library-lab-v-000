e9966efda247777b579eb58d8b7b2077905ff4e3
Matt Etress <mattetress@me.com>
2018-10-17 08:45:01 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO series (title, author_id, subgenre_id) VALUES 
+INSERT INTO series (title, author_id, subgenre_id) VALUES
 ("Harry Potter", 1, 1),
 ("Lord of the Rings", 2, 2);
 
@@ -30,19 +30,5 @@ INSERT INTO (name, motto, species, author_id, series_id) VALUES
 
 
 INSERT INTO character_books (character_id, book_id) VALUES
-(1, 1),
-(1, 2),
-(1, 3),
-(2, 1),
-(2, 2),
-(2, 3),
-(3, 1),
-(4, 3),
-(5, 4),
-(5, 5),
-(5, 6),
-(6, 4),
-(6, 5),
-(6, 6),
-(7, 5),
-(8, 6);
+(1, 1), (1, 2), (1, 3), (2, 1), (2, 2), (2, 3), (3, 1), (4, 3),
+(5, 4), (5, 5), (5, 6), (6, 4), (6, 5), (6, 6), (7, 5), (8, 6);
