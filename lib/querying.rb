2dcf00e12a94f0a39898e7e6c54e64be663e6e8f
Matt Etress <mattetress@me.com>
2018-10-17 09:16:36 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -37,5 +37,9 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(books.name)
+  FROM characters JOIN character_books ON characters.id = character_books.character_id
+  JOIN books ON character_books.book_id = books.id
+  GROUP BY characters.name
+  ORDER BY COUNT(books.name) DESC;"
 end
